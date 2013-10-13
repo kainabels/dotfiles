11300c942baeee43066852ec138c5eaec1c9259f
kainabels <kain@frgmnt.info>
2013-10-13 21:56:01 +0900

    Add alias and path for MacVim.

--- zsh/my.zsh
+++ zsh/my.zsh
@@ -2,3 +2,6 @@ unsetopt correct_all
 #alias -g ...='../..'
 fpath=(/usr/local/share/zsh-completions $fpath)
 
+alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+
