d855d3ea59cea59f1d2ac3f93c80e252c28468eb
kainabels <kain@frgmnt.info>
2013-10-20 10:13:17 +0900

    [zsh][bash] Add rbenv settings.

--- zsh/my.zsh
+++ zsh/my.zsh
@@ -5,3 +5,5 @@ fpath=(/usr/local/share/zsh-completions $fpath)
 alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 
+eval "$(SHELL=zsh rbenv init -)"
+
