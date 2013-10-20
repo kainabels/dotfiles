acb6f467f17e9ecd1e503b644c9d60448517cb6f
kainabels <kain@frgmnt.info>
2013-10-20 13:11:23 +0900

    [zsh][bash] check rbenv before rbenv init.

--- zsh/my.zsh
+++ zsh/my.zsh
@@ -5,5 +5,7 @@ fpath=(/usr/local/share/zsh-completions $fpath)
 alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 
-eval "$(SHELL=zsh rbenv init -)"
+if which rbenv >/dev/null; then
+	eval "$(rbenv init - zsh)"
+fi
 
