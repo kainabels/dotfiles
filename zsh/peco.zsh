14477142d61ee4fbcdfa263322d8f697d8b88f9c
kainabels <kain@frgmnt.info>
2015-06-14 02:07:28 +0900

    [zsh] Reindent to 2.

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -1,32 +1,32 @@
 function ppgrep() {
-    if [[ $1 == "" ]]; then
-        PECO=peco
-    else
-        PECO="peco --query $1"
-    fi
-    ps aux | eval $PECO | awk '{ print $2 }'
+	if [[ $1 == "" ]]; then
+		PECO=peco
+	else
+		PECO="peco --query $1"
+	fi
+	ps aux | eval $PECO | awk '{ print $2 }'
 }
 
 function ppkill() {
-    if [[ $1 =~ "^-" ]]; then
-        QUERY=""            # options only
-    else
-        QUERY=$1            # with a query
-        [[ $# > 0 ]] && shift
-    fi
-    ppgrep $QUERY | xargs kill $*
+	if [[ $1 =~ "^-" ]]; then
+		QUERY=""            # options only
+	else
+		QUERY=$1            # with a query
+		[[ $# > 0 ]] && shift
+	fi
+	ppgrep $QUERY | xargs kill $*
 }
 
 function exists { which $1 &> /dev/null }
 
 if exists peco; then
-    function peco_select_history() {
-        local tac
-        exists gtac && tac="gtac" || { exists tac && tac="tac" || { tac="tail -r" } }
-        BUFFER=$(fc -l -n 1 | eval $tac | peco --query "$LBUFFER")
-        CURSOR=$#BUFFER         # move cursor
-        zle -R -c               # refresh
-    }
+	function peco_select_history() {
+		local tac
+		exists gtac && tac="gtac" || { exists tac && tac="tac" || { tac="tail -r" } }
+		BUFFER=$(fc -l -n 1 | eval $tac | peco --query "$LBUFFER")
+		CURSOR=$#BUFFER         # move cursor
+		zle -R -c               # refresh
+	}
 
     zle -N peco_select_history
     bindkey '^R' peco_select_history
